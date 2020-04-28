158ee5156e0288afd60ad76decb1cf0234bd3d61
gudzek <64497034+gudzek@users.noreply.github.com>
2020-04-28 21:51:44 +0200

    Add files via upload

--- program4.py
+++ program4.py
@@ -1,8 +1,6 @@
-import math
-
 """
-Program do rozwiązywania układów nieliniowych
-Copyright (C) 2018 by Damian Guzek, Emilia Koniszewska and Dawid Drzymalski.
+Program do rozwiązywania równań różniczkowych cząstkowych
+Copyright (C) 2019 by Damian Guzek, Emilia Koniszewska and Dawid Drzymalski.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,10 +13,10 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details,
 available at <https://www.gnu.org/licenses/>.
 
-Started on December 27, 2018. Last revision: December 28, 2018.
+Started on January 20, 2019. Last revision: 2019.
 
-Podczas pisana programu korzystalismy z zasobów poniższej strony
-https://www.math.ubc.ca/~pwalls/
+Podczas pisana programu korzystalismy z zasobów:
+* Książka D. Kincaid, W. Cheney: Analiza numeryczna, WNT, Warszawa 2006, ISBN 83-204-3078-X - Rozdział 9
 """
 
 
@@ -26,151 +24,99 @@ https://www.math.ubc.ca/~pwalls/
 
 
 title = "\
-Program3 v. 1.00, 28/12/2018. Copyright (C) by Damian Guzek, Emilia Koniszewska and Dawid Drzymalski.\n\
+Program3 v. 1.00, 20/01/2019. Copyright (C) by Damian Guzek, Emilia Koniszewska and Dawid Drzymalski.\n\
 This is free software. No warranty. See GNU GPL for details.\n";
 #
 
-#######################################################################
-### Definicje funkcji - part 0
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Metoda bisekcji
-def bisection(f,a,b,N):
-    if f(a)*f(b) >= 0:
-        print("Bisection method fails.")
-        return None
-    a_n = a
-    b_n = b
-    for n in range(1,N+1):
-        m_n = (a_n + b_n)/2
-        f_m_n = f(m_n)
-        if f(a_n)*f_m_n < 0:
-            a_n = a_n
-            b_n = m_n
-        elif f(b_n)*f_m_n < 0:
-            a_n = m_n
-            b_n = b_n
-        elif f_m_n == 0:
-            print("Found exact solution.")
-            return m_n
-        else:
-            print("Bisection method fails.")
-            return None
-    return (a_n + b_n)/2
+import matplotlib.pyplot as plt
+import math
+################################################################################################################
+#Program oblicza metodą jawną przybliżenie funkcji będącej rozwiązaniem u_xx + u_tt=0, dla u(x,0)=g(x),
+#u(0,t)=a(t) u(1,t)=b(t). Rozwiązanie opisane jest na [0,1]x[0,T], gdzie T = h*M
+################################################################################################################
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Metoda siecznych
-def secant(f,a,b,N):
-    if f(a)*f(b) >= 0:
-        print("Secant method fails.")
-        return None
-    a_n = a
-    b_n = b
-    for n in range(1,N+1):
-        m_n = a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
-        f_m_n = f(m_n)
-        if f(a_n)*f_m_n < 0:
-            a_n = a_n
-            b_n = m_n
-        elif f(b_n)*f_m_n < 0:
-            a_n = m_n
-            b_n = b_n
-        elif f_m_n == 0:
-            print("Found exact solution.")
-            return m_n
-        else:
-            print("Secant method fails.")
-            return None
-    return a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+#Program został sprawdzony na kilku przykładach, aby sprawdzić wpływ zmiany wartości s na dokładność rozwiązania
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Metoda netwona
-def newton(f,Df,x0,epsilon,max_iter):
-    xn = x0
-    for n in range(1,max_iter):
-        fxn = f(xn)
-        if abs(fxn) < epsilon:
-            print('Found solution after',n,'iterations.')
-            return xn
-        Dfxn = Df(xn)
-        if Dfxn == 0:
-            print('Zero derivative. No solution found.')
-            return None
-        xn = xn - fxn/Dfxn
-        print(xn)
-    print('Exceeded maximum iterations. No solution found.')
-    return None
+#Aby sprawdzić konkretny przypadek, proszę odkomentować odpowiednia wartości k i M
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Wzory funckcji - part 0.5
-f = lambda x: x**3 - x**2 - 1
-Df = lambda x: 3*x**2 - 2*x
+#Dane do sprawdzenia z książki "Analiza numeryczna" - Zadanie 9.1
+n = 9
+k = 0.005125
+M = 200
 
-w = lambda x: x**2 - x - 1
-Dw = lambda x: 2*x -1
+#k = 0.006
+#M = 171
 
-g = lambda x: math.sin(x - 1.4)
-Dg = lambda x: math.cos(x - 1.4)
+#Porównujemy, jak zmiana M wpływa na wynik
+#k = 0.005
+#M = 200
+#M = 50
 
-
-
+#Sprawdzamy wynik dla s = 0,549999
+#k = 0.0055
+#M = 200
 
 #######################################################################
-### Wykonywanie kilkudziesięciu iteracji - part 1
-print("funkcja f")
-print("Metoda bisekcji")
-for i in range(1,50):
-    approx_fb = bisection(f,1,2,i)
-    print(approx_fb)
+#Zmiana na n = 50
+#n = 50
 
-print("\n\n")
-print("funkcja f")
-print("Metoda siecznych")
-for i in range(1,50):
-    approx_fs = secant(f,1,2,i)
-    print(approx_fs)
+#k = 0.00015
+#M = 200
 
-print("\n\n")
-print("funkcja f")
-print("Metoda Netwona")
-approx_fn = newton(f,Df,1,1e-17,50)
-print(approx_fn)
+#Pomimo s = 0.54621, wynik dokładny zwłaszcza dla x<0.5
+#k = 0.00021
+#M = 200
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-print("funkcja w")
-print("Metoda bisekcji")
-for i in range(1,50):
-    approx_wb = bisection(w,1,2,i)
-    print(approx_wb)
+#Po raz kolejny po zwiększeniu M otrzymujemy wyniki odbiegające od wyniku rzeczywistego
+#k = 0.00021
+#M = 500
 
-print("\n\n")
-print("funkcja w")
-print("Metoda siecznych")
-for i in range(1,50):
-    approx_ws = secant(w,1,2,i)
-    print(approx_ws)
+#Kiedy s < 0.5 i duże n, możemy przyjąć nawet M = 2000 (również sprawdzone na większych wartościach) i wynik pozostaje bardzo dokładny
+#k = 0.0001
+#M = 2000
 
-print("\n\n")
-print("funkcja w")
-print("Metoda Netwona")
-approx_wn = newton(w,Dw,1,1e-17,50)
-print(approx_wn)
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-print("funkcja g")
-print("Metoda bisekcji")
-for i in range(1,50):
-    approx_gb = bisection(g,1,2,i)
-    print(approx_gb)
+#################################################################################################
+#Określamy potrzebne tablice i
+v = [0]*(n+2)
+u = [0]*(n+2)
+w = [0]*(n+2)
+h = 1/(n+1)
+s = k/(h**2)
 
-print("\n\n")
-print("funkcja g")
-print("Metoda siecznych")
-for i in range(1,50):
-    approx_gs = secant(g,1,2,i)
-    print(approx_gs)
+#Warunek u(x,0)
+for i in range(0, n+2):
+    w[i] = float(math.sin(i*h*math.pi))
 
-print("\n\n")
-print("funkcja g")
-print("Metoda Netwona")
-approx_gn = newton(g,Dg,1,1e-17,50)
-print(approx_gn)
+t = 0
+#print(0,t,w)
+
+#Algorytm do wyliczenia funkcji przybliżonej przy pomocy metody jawnej
+for j in range(1, M+1):
+    t = j*k
+    v[0] = 0
+    v[n+1] = 0
+    for i in range(1, n+1):
+        v[i] = float(s*w[i-1] + (1-2*s)*w[i] + s*w[i+1])
+    #print(j,t,v)
+
+    for i in range(0, n+2):
+        w[i] = v[i]
+
+#Pętla potrzebna do wyliczenia rozwiązania dokładnego
+x = [0]*(n+2)
+for i in range(0, n + 2):       #obliczenie wartości dokładnych znając rozwiązanie zagadnienia
+    x[i] = i*h
+    u[i] = float(math.exp(-(math.pi)*(math.pi)*M*k)*math.sin((math.pi)*x[i]))
+
+#Pokazanie wyników - głównie potrzebne do analizy
+print(w)
+print(u)
+print(s)
+
+#Wizualna prezentacja otrzymanych wyników
+plt.plot(x, u,'#32CD32', label="Wynik dokładny")        #rysowanie wykresu porównującego otrzymane wyniki
+plt.plot(x, v,'#00ACEE', label="Metoda jawna")
+plt.legend(loc='upper right')#Tworzenie legendy rysowanych wykresów
+plt.grid(True)
+plt.show()
